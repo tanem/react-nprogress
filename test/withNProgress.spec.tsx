0a3db70886cb5d75b146eaf26ca493f7f02d365b
Tane Morgan <tane.morgan@gmail.com>
2019-03-01 15:41:18 +1300

    Tidy up HOC tests

--- test/withNProgress.spec.tsx
+++ test/withNProgress.spec.tsx
@@ -1,23 +1,16 @@
 import { mount } from 'enzyme'
 import createMockRaf from 'mock-raf'
 import * as React from 'react'
-import { NProgress, RenderProps, withNProgress } from '../src'
+import { withNProgress } from '../src'
 
 const mockRaf = createMockRaf()
 window.requestAnimationFrame = mockRaf.raf
 window.cancelAnimationFrame = mockRaf.cancel
 
-// TODO: use test cases to prevent duplicating render prop tests?
-// is it only the render method that will change?
-
-const Child: React.SFC<RenderProps> = () => <div />
+const Child: React.SFC = () => <div />
 
 let wrapper: any
-let EnhancedComponent: any
-
-beforeEach(() => {
-  EnhancedComponent = withNProgress(Child)
-})
+const EnhancedComponent = withNProgress(Child)
 
 afterEach(() => wrapper.unmount())
 
@@ -25,7 +18,7 @@ test('does not animate on mount if isAnimating is false', () => {
   wrapper = mount(<EnhancedComponent />)
 
   expect(wrapper.find(Child).props()).toEqual({
-    animationDuration: wrapper.find(NProgress).prop('animationDuration'),
+    animationDuration: wrapper.find(Child).prop('animationDuration'),
     isFinished: false,
     progress: 0
   })
@@ -35,7 +28,7 @@ test('starts animating on mount if isAnimating is true', () => {
   wrapper = mount(<EnhancedComponent isAnimating />)
 
   expect(wrapper.find(Child).props()).toEqual({
-    animationDuration: wrapper.find(NProgress).prop('animationDuration'),
+    animationDuration: 200,
     isAnimating: true,
     isFinished: false,
     progress: 0.1
@@ -49,7 +42,7 @@ test('starts animating on mount if isAnimating was changed from false to true', 
   wrapper.update()
 
   expect(wrapper.find(Child).props()).toEqual({
-    animationDuration: wrapper.find(NProgress).prop('animationDuration'),
+    animationDuration: 200,
     isAnimating: true,
     isFinished: false,
     progress: 0.1
@@ -60,14 +53,14 @@ test('increments correctly', () => {
   wrapper = mount(<EnhancedComponent isAnimating />)
 
   mockRaf.step()
-  mockRaf.step({ time: wrapper.find(NProgress).prop('animationDuration') + 1 })
+  mockRaf.step({ time: 201 })
   mockRaf.step()
-  mockRaf.step({ time: wrapper.find(NProgress).prop('incrementDuration') + 1 })
+  mockRaf.step({ time: 801 })
 
   wrapper.update()
 
   expect(wrapper.find(Child).props()).toEqual({
-    animationDuration: wrapper.find(NProgress).prop('animationDuration'),
+    animationDuration: 200,
     isAnimating: true,
     isFinished: false,
     progress: 0.2
@@ -80,12 +73,12 @@ test('animates to finish if isAnimating was changed from true to false', () => {
   wrapper.setProps({ isAnimating: false })
 
   mockRaf.step()
-  mockRaf.step({ time: wrapper.find(NProgress).prop('animationDuration') + 1 })
+  mockRaf.step({ time: 201 })
 
   wrapper.update()
 
   expect(wrapper.find(Child).props()).toEqual({
-    animationDuration: wrapper.find(NProgress).prop('animationDuration'),
+    animationDuration: 200,
     isAnimating: false,
     isFinished: true,
     progress: 1
