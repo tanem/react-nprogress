f69fa7670468369f760c0d743dd68304b47f316c
Tane Morgan <tane.morgan@gmail.com>
2019-03-02 08:20:36 +1300

    Fix imports in examples

--- examples/original-design/src/index.js
+++ examples/original-design/src/index.js
@@ -1,4 +1,4 @@
-import NProgress from '@tanem/react-nprogress'
+import { NProgress } from '@tanem/react-nprogress'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Bar from './Bar'
