ccb08ad66c023000dad9d1efb1749b79c91bb5b4
Tane Morgan <464864+tanem@users.noreply.github.com>
2020-11-14 06:49:26 +1300

    Update next-router example

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,8 @@ module.exports = {
   ],
   plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
   },
@@ -30,6 +32,12 @@ module.exports = {
       rules: {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+    {
+      files: ['examples/next-router/**/*'],
+      rules: {
+        'react/prop-types': 'off',
       },
     },
   ],
