97c2eafa6076f4138f85f7606d165d1611eaeabc
Tane Morgan <464864+tanem@users.noreply.github.com>
2020-11-22 10:13:59 +1300

    Add render-props example

---
+++ examples/render-props/src/Progress.tsx
@@ -0,0 +1,19 @@
+import { NProgress } from '@tanem/react-nprogress'
+import React from 'react'
+
+import Bar from './Bar'
+import Container from './Container'
+import Spinner from './Spinner'
+
+const Progress: React.FC<{ isAnimating: boolean }> = ({ isAnimating }) => (
+  <NProgress isAnimating={isAnimating}>
+    {({ animationDuration, isFinished, progress }) => (
+      <Container animationDuration={animationDuration} isFinished={isFinished}>
+        <Bar animationDuration={animationDuration} progress={progress} />
+        <Spinner />
+      </Container>
+    )}
+  </NProgress>
+)
+
+export default Progress
---
+++ examples/render-props/src/index.tsx
@@ -0,0 +1,31 @@
+import './index.css'
+
+import React, { useEffect, useState } from 'react'
+import ReactDOM from 'react-dom'
+
+import Progress from './Progress'
+
+const callFakeAPI = (delay: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
+
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    ;(async () => {
+      await callFakeAPI(3000)
+      setIsLoading(false)
+    })()
+  }, [])
+
+  return (
+    <React.Fragment>
+      <Progress isAnimating={isLoading} />
+      <h1>{isLoading ? 'Loading...' : 'Loaded!'}</h1>
+    </React.Fragment>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
