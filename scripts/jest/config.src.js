24319021632fd5c1eddbfc59d70c67c6ead8f3b8
Tane Morgan <464864+tanem@users.noreply.github.com>
2021-06-09 06:31:16 +1200

    Update jest deps

--- scripts/jest/config.src.js
+++ scripts/jest/config.src.js
@@ -5,6 +5,7 @@ module.exports = {
   preset: 'ts-jest',
   rootDir: process.cwd(),
   roots: ['<rootDir>/test'],
+  testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/test/*.spec.ts?(x)'],
   transform: { '^.+\\.(js|tsx?)$': 'ts-jest' },
 }
