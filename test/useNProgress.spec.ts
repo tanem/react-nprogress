5828c3e9cd503b1f7fe8409614b47ddd79af6805
Tane Morgan <464864+tanem@users.noreply.github.com>
2022-05-20 05:57:09 +1200

    Drop react-hooks-testing-library

--- test/useNProgress.spec.ts
+++ test/useNProgress.spec.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks'
+import { act, renderHook } from '@testing-library/react'
 import createMockRaf from 'mock-raf'
 
 import { useNProgress } from '../src'
