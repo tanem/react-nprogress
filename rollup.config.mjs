69382cc25cd753da8b1f786fd7fdc86be67752a8
Tane Morgan <464864+tanem@users.noreply.github.com>
2024-11-20 06:02:19 +1300

    Fix syntax error in rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 import sourcemaps from 'rollup-plugin-sourcemaps'
 
-import pkg from './package.json' assert { type: 'json' }
+import pkg from './package.json' with { type: 'json' }
 
 const CJS_DEV = 'CJS_DEV'
 const CJS_PROD = 'CJS_PROD'
@@ -59,7 +59,7 @@ const getPlugins = (bundleType) => [
   replace({
     preventAssignment: true,
     'process.env.NODE_ENV': JSON.stringify(
-      isProduction(bundleType) ? 'production' : 'development'
+      isProduction(bundleType) ? 'production' : 'development',
     ),
   }),
   sourcemaps(),
